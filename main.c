88f5f6ba1399da6fbdd361e475ce2a1a1d7bc8b6
AdamDS98 <adam.solomons1998@gmail.com>
2020-03-03 14:14:32 +1100

    changed equation within code to practice using Git

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     printf("Enter a number: ");
     scanf("%f", &x); // Note change of %d to %f
     //Complete the y = line below
-    y = (x + 2) / (x-1);
+    y = x + 2;
     printf("y: %f\n", y);
     return 0;
 }
